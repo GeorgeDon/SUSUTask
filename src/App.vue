12a1c25e0c59e3f1f85ee5dee4086f414b3d2549
GeorgeDon <826481939@qq.com>
2018-08-10 00:14:30 +0800

    首页框架

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,71 @@
 <template>
-  <div id="app">
-    <myCalendar/>
-    <router-view/>
+  <div>
+    <div id="main_menu">
+
+    </div>
+    <div id="app" class="home">
+      <div class="left_menu"></div>
+      <div class="project">
+        <router-view/>
+      </div>
+      <div class="support">
+        <myCalendar/>
+      </div>
+
+    </div>
+    <div id="foot">
+
+    </div>    
   </div>
 </template>
 
 <script>
-import myCalendar from '@/components/myCalendar'
+import myCalendar from "@/components/myCalendar";
 export default {
-  name: 'App',
-  components : {myCalendar}
-}
+  name: "App",
+  components: { myCalendar }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#main_menu {
+  height: 60px;
+  background-color: aqua;
+}
+#foot {
+  position: absolute;
+  top: 95%;
+  right: 0;
+  left: 0;
+  bottom: 0px;
+  background-color: yellow;
+  border: 1px solid black;
+}
+.home{
+  position: absolute;
+  top: 60px;
+  right: 0;
+  left: 0;
+  bottom: 5%;
+  border: 3px solid black;
+}
+.left_menu{
+    width: 10%;
+    float: left;
+    border: 1px solid black;
+    background-color:green;
+    height: 100%;
+}
+.project{
+  border: 1px solid pink;
+  float: left;
+  height: 100%;
+  width: 74%;
+}
+.support{
+  width: 15%;
+  float: right;
+  background-color: red;
+  border: 1px solid black;
 }
 </style>
