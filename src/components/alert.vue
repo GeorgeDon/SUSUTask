368643ee7af2141942f218d0f3d9fa9a8c8ba958
leisusu <1187946608@qq.com>
2018-08-09 22:30:04 +0800

    ““弹窗”

---
+++ src/components/alert.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class='md-cont' v-show='inshowstate'>
+        <div class='md-wrapper' >
+            <div class='md-title'>
+            <slot>
+                {{title}}
+            </slot>
+            </div>
+            <div class='md-text'>
+            <slot name='text' >
+                {{msg}}
+            </slot>
+            </div>
+            <div>
+                <button @click='clickOK'>"确定"</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "alert",
+  props: {
+      intitle:String,
+      inmsg:String,
+      inshowstate:Boolean
+  },
+  data () {
+    return {
+      title:this.intitle,
+      msg:this.inmsg
+    }
+  },
+
+  methods: {
+    clickOK: function() {
+      this.inshowstate=false;
+    },
+  }
+};
+</script>
+<style scoped>
+
+</style>
--- src/components/myCalendar.vue
+++ src/components/myCalendar.vue
@@ -3,24 +3,39 @@
     <Calendar
       v-on:choseDay="clickDay"
       v-on:changeMonth="changeDate"
+      
     ></Calendar>
+    
+    <!-- <alert :inshowstate="show" :intitle="alertTitle" :inmsg="alertMsg"></alert> -->
+    <dialog-bar v-model="sendVal" type="danger" :title="alertTitle" :content="alertMsg" v-on:cancel="clickCancel()" @danger="clickDanger()" @confirm="clickConfirm()" dangerText="Delete"></dialog-bar>
   </div>
 </template>
 
 <script>
-import Calendar from 'vue-calendar-component';
+import Calendar from "vue-calendar-component";
+// import alert from "@/components/alert";
+import dialogBar from "@/components/myDialog.vue";
+
 export default {
-  name: 'myCalendar',
-  data () {
+  name: "myCalendar",
+  data() {
     return {
-      
-    }
+      show: false,
+      alertTitle: "提示",
+      alertMsg: "这是一个弹框",
+      sendVal: false
+    };
   },
   components: {
-    Calendar
+    Calendar,
+    alert,
+    "dialog-bar": dialogBar
   },
-  methods:{
+  methods: {
     clickDay(data) {
+      // this.show = true;//弹框
+      // console.log(this.show);
+      this.sendVal=true;
       console.log(data); //选中某天
     },
     changeDate(data) {
@@ -28,12 +43,24 @@ export default {
     },
     clickToday(data) {
       console.log(data); //跳到了本月
+    },
+    openMask(index) {
+      this.sendVal = true;
+      console.log(this.sendVal)
+    },
+    clickCancel() {
+      console.log("点击了取消");
+    },
+    clickDanger() {
+      console.log("这里是danger回调");
+    },
+    clickConfirm() {
+      console.log("点击了confirm");
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
